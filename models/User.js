e1637c1dd52dfd43ec999f4582c173892b197339
IMRANDIL <aliimranadil2@gmail.com>
2022-07-30 18:04:48 +0530

    yh

--- models/User.js
+++ models/User.js
@@ -1,5 +1,29 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      default: null,
+    },
+    lastname: {
+      type: String,
+      default: null,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    token: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", UserSchema);
